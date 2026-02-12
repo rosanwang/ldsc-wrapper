a7bbc0c02af196c813d5074cd6368fa2f81fc773
rosanwang <72318853+rosanwang@users.noreply.github.com>
2026-02-12 15:51:13 -0800

    Delete sumstats/generate.sumstats.sh

--- sumstats/generate.sumstats.sh
+++
@@ -1,7 +0,0 @@
-conda activate /anvil/scratch/x-rwang22/envs/ldsc
-
-INDIR=/anvil/scratch/x-rwang22/GWAS
-OUTDIR=/anvil/projects/x-mcb130189/rwang22/bican/ref/gwas_traits
-sbatch -p shared --mem=75g --time=20:00:00 --nodes=1 --wrap="/home/x-rwang22/ldsc/munge_sumstats.py --sumstats ${INDIR}/Bellenguez.NatGenet.2022.Alzheimers_Disease_Dementia.formatted.tsv --out ${OUTDIR}/Bellenguez.NatGenet.2022.Alzheimers_Disease_Dementia"
-sbatch -p shared --mem=75g --time=20:00:00 --nodes=1 --wrap="/home/x-rwang22/ldsc/munge_sumstats.py --sumstats ${INDIR}/Mullins.NatGenet.2021.Bipolar_Disorder.formatted.tsv --out ${OUTDIR}/Mullins.NatGenet.2021.Bipolar_Disorder"
-sbatch -p shared --mem=75g --time=20:00:00 --nodes=1 --wrap="/home/x-rwang22/ldsc/munge_sumstats.py --sumstats ${INDIR}/Wightman.NatGenet.2021.Alzheimers.formatted.tsv --out ${OUTDIR}/Wightman.NatGenet.2021.Alzheimers"
